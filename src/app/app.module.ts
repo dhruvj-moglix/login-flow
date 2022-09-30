077523a4d5ff2c30072ff3ed2cf38ef0fd589634
Dhruv <dhruv.jain@moglix.com>
2022-09-30 13:14:14 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { TitleComponent } from './title/title.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,57 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormGroup,FormControl } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 import { HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  title: string = '';
+  token: boolean = false;
 
-  constructor(private http:HttpClient) { }
-  title:string='';
-  token:any='';
+  loginForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    phoneNumber: new FormControl(''),
+  });
 
-  loginForm=new FormGroup({
-    name :  new FormControl(''),
-    email :new FormControl(''),
-    password:new FormControl(''),
-    phoneNumber:new FormControl('')
-  })
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  postBody():any{
-    let postBody = Object.assign({},this.loginForm.value);
-    postBody['name']=this.loginForm.controls['name'].value;
-    postBody['email']=this.loginForm.controls['email'].value;
-    postBody['password']=this.loginForm.controls['password'].value;
-    postBody['phoneNumber']=this.loginForm.controls['phoneNumber'].value;
+  postBody(): any {
+    let postBody = Object.assign({}, this.loginForm.value);
+    postBody['name'] = this.loginForm.controls['name'].value;
+    postBody['email'] = this.loginForm.controls['email'].value;
+    postBody['password'] = this.loginForm.controls['password'].value;
+    postBody['phoneNumber'] = this.loginForm.controls['phoneNumber'].value;
 
     return postBody;
   }
 
-  showData(){
-    this.getToken();
+  getToken() {
+    this.http
+      .post(
+        'https://expense-tracker-service.herokuapp.com/api/v1/user/register',
+        this.postBody()
+      )
+      .subscribe((result: any) => {
+        console.log(result);
+        localStorage.setItem('token',result.token);
+        this.token=true;
+      });
   }
 
-  getToken(){
-    this.http.post("https://expense-tracker-service.herokuapp.com/api/v1/user/register",this.postBody())
-    .subscribe(result=>{
-      console.log(result);
-      // this.token=result.token;
-    });
-
-
-  }
-
-   headers = new HttpHeaders({ 'Authorization': this.token })
-  titleAdd(){
-    this.http.post("https://expense-tracker-service.herokuapp.com/api/v1/user/transaction/category",
-    {"title":this.title},{headers:this.headers}).subscribe(res=>{
-      console.log(res);
-    })
+  titleAdd() {
+    const headers = new HttpHeaders({ Authorization: `${localStorage.getItem('token')}`});
+    this.http
+      .post(
+        'https://expense-tracker-service.herokuapp.com/api/v1/user/transaction/category',
+        { title: this.title },
+        { headers: headers }
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
 }
